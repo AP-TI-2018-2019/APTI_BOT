114c59badc5201609bcad0ab3e3697f4961158ce
mixxamm <maximjanssensj5@gmail.com>
2019-07-03 00:19:04 +0200

    Bereken herinnering tijd
    Al een basis code voor het berekenen van de datum en tijd wanneer de herinnering moet worden verstuurd. Momenteel is het enkel mogelijk om de herinnering over een aantal seconden, minuten, uren, etc. te zetten. Doel is om ook bijvoorbeeld "volgende dinsdag" etc. te kunnen zeggen of gewoon een datum. Verder ook nog zorgen dat de herinnering effectief op het juiste moment wordt verstuurd.

---
+++ APTI_BOT/GeenGetalException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APTI_BOT
+{
+    class GeenGetalException : Exception
+    {
+        public GeenGetalException() : base("Je hebt geen getal meegegeven")
+        {
+        }
+    }
+}
--- APTI_BOT/Program.cs
+++ APTI_BOT/Program.cs
@@ -6,11 +6,13 @@ using System;
 using System.Threading.Tasks;
 using System.IO;
 using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 
 namespace APTI_BOT
 {
     class Program
     {
+        const string HERINNERCOMMAND = "herrinner me";
         private DiscordSocketClient _client;
         public static void Main(string[] args)
         => new Program().MainAsync().GetAwaiter().GetResult();
@@ -97,9 +99,41 @@ namespace APTI_BOT
             emoji[0] = new Emoji("🥇");
             emoji[1] = new Emoji("🥈");
             emoji[2] = new Emoji("🥉");
-            if (message.Content == "!AP")
+            if (message.Content.Contains("!AP"))
             {
                 await message.Channel.SendMessageAsync("TI!");
+            }
+            else if (message.Content.Contains("herinner me"))
+            {
+                await message.Channel.SendMessageAsync("oke");
+                string herinnering = message.Content.Substring(HERINNERCOMMAND.Length);
+                int startHerinnering = herinnering.IndexOf(' ');
+                startHerinnering = herinnering.IndexOf(' ', startHerinnering + 1);
+                herinnering = herinnering.Substring(startHerinnering);
+                if (herinnering.Contains("seconden") || herinnering.Contains("second") || herinnering.Contains("minuten") || herinnering.Contains("minuut") || herinnering.Contains("uren") || herinnering.Contains("uur") || herinnering.Contains("dagen") || herinnering.Contains("dag") || herinnering.Contains("jaar") || herinnering.Contains("jaren") || herinnering.Contains("maanden") || herinnering.Contains("maand"))
+                {
+                    int secondIndex = herinnering.LastIndexOf("seconden") != -1 ? herinnering.LastIndexOf("seconden") : herinnering.LastIndexOf("second");
+                    int minuutIndex = herinnering.LastIndexOf("minuten") != -1 ? herinnering.LastIndexOf("minuten") : herinnering.LastIndexOf("minuut");
+                    int uurIndex = herinnering.LastIndexOf("uren") != -1 ? herinnering.LastIndexOf("uren") : herinnering.LastIndexOf("uur");
+                    int dagIndex = herinnering.LastIndexOf("dagen") != -1 ? herinnering.LastIndexOf("dagen") : herinnering.LastIndexOf("dag");
+                    int weekIndex = herinnering.LastIndexOf("week") != -1 ? herinnering.LastIndexOf("week") : herinnering.LastIndexOf("weken");
+                    int maandIndex = herinnering.LastIndexOf("maanden") != -1 ? herinnering.LastIndexOf("maanden") : herinnering.LastIndexOf("maand");
+                    int jaarIndex = herinnering.LastIndexOf("jaren") != -1 ? herinnering.LastIndexOf("jaren") : herinnering.LastIndexOf("jaar");
+                    Console.WriteLine(maandIndex);
+                    Console.WriteLine(uurIndex);
+                    int seconden = KrijgAantal(herinnering, secondIndex != -1 ? secondIndex : 0);
+                    int minuten = KrijgAantal(herinnering, minuutIndex != -1 ? minuutIndex : 0);
+                    int uren = KrijgAantal(herinnering, uurIndex != -1 ? uurIndex : 0);
+                    int dagen = KrijgAantal(herinnering, dagIndex != -1 ? dagIndex : 0);
+                    int weken = KrijgAantal(herinnering, weekIndex != -1 ? weekIndex : 0);
+                    int maanden = KrijgAantal(herinnering, maandIndex != -1 ? maandIndex : 0);
+                    int jaren = KrijgAantal(herinnering, jaarIndex != -1 ? jaarIndex : 0);
+                    Console.WriteLine(maanden);
+                    Console.WriteLine(uren);
+                    DateTime datumHerinnering = DateTime.Now.AddSeconds(seconden).AddMinutes(minuten).AddHours(uren).AddDays(dagen).AddMonths(maanden).AddYears(jaren);
+                    Console.WriteLine(datumHerinnering);
+                    await message.Channel.SendMessageAsync($"Herinnering om {herinnering.Substring(0, herinnering.LastIndexOf("over") != -1? herinnering.LastIndexOf("over") : herinnering.LastIndexOf("binnen"))} ingesteld op {datumHerinnering.ToShortDateString()} om {datumHerinnering.ToShortTimeString()}.");
+                }
             }
             else if (message.Content == "!date")
             {
@@ -127,7 +161,7 @@ namespace APTI_BOT
                 string naam = message.Content.Substring(5);
                 var guild = _client.GetGuild(config.ServerId);
                 var user = guild.GetUser(message.Author.Id);
-                Console.Write(message.Content);   
+                Console.Write(message.Content);
                 try
                 {
                     await user.ModifyAsync(x =>
@@ -137,9 +171,9 @@ namespace APTI_BOT
                     var sent = await message.Author.SendMessageAsync($"Je nickname is ingesteld op {naam}. De volgende stap is je jaar kiezen door te klikken op één (of meerdere) emoji onder dit bericht. Als je vakken moet meenemen, dan kan je ook het vorige jaar kiezen. Als je geen kanalen meer wilt zien van een jaar dan kan je gewoon opnieuw op de emoji ervan klikken. Als je jaar niet verandert dan is de sessie van deze chat verlopen en moet je de sessie terug activeren door `!jaar` te typen.");
                     await sent.AddReactionsAsync(emoji);
                 }
-                catch(Discord.Net.HttpException e)
+                catch (Discord.Net.HttpException e)
                 {
-                    if(e.HttpCode == System.Net.HttpStatusCode.Forbidden)
+                    if (e.HttpCode == System.Net.HttpStatusCode.Forbidden)
                     {
                         var sent_error = await message.Author.SendMessageAsync("Ik heb niet de machtigingen om jouw naam te veranderen, je zal dit zelf moeten doen. Als troost mag je wel kiezen in welk jaar je zit :)");
                         await sent_error.AddReactionsAsync(emoji);
@@ -151,7 +185,7 @@ namespace APTI_BOT
                     }
                 }
             }
-            else if(message.Channel is IPrivateChannel && message.Source == MessageSource.User && message.Content == "!jaar")
+            else if (message.Channel is IPrivateChannel && message.Source == MessageSource.User && message.Content == "!jaar")
             {
                 var sent = await message.Author.SendMessageAsync("Kies je jaar door op één of meer van de emoji onder dit bericht te klikken.");
                 await sent.AddReactionsAsync(emoji);
@@ -171,7 +205,8 @@ namespace APTI_BOT
             else if (message.Content == "!discord" || message.Content == "!dc")
             {
                 await message.Channel.SendMessageAsync("https://apti.ml/discord");
-            }else if(message.Content == "!help")
+            }
+            else if (message.Content == "!help")
             {
                 await message.Channel.SendMessageAsync("!AP - TI!\n!site - geeft link naar onze site\n!github | !gh - geeft link naar onze GitHub-repo\n!youtube | !yt - geeft link naar ons YouTube-kanaal\n!discord | !dc - geeft link naar onze Discord-uitnodigingspagina\n!date - geeft de datum van vandaag\n!time - geeft de huidige tijd\n!datetime - geeft de huidige datum en tijd");
             }
@@ -264,6 +299,28 @@ namespace APTI_BOT
                 //    }
                 //}
             }
+        }
+
+        private static int KrijgAantal(string bericht, int eindIndex)
+        {
+            try
+            {
+                bericht = bericht.Substring(0, eindIndex).TrimEnd(' ');
+                if (Regex.IsMatch(bericht[bericht.Length - 1].ToString(), @"[0-9]"))
+                    return ExtractAantal(bericht, eindIndex);
+                return 0;
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                Console.WriteLine("test");
+                return 0;
+            }
+        }
+
+        private static int ExtractAantal(string bericht, int eindIndex)
+        {
+            int startIndex = bericht.LastIndexOf(' ');
+            return int.Parse(bericht.Substring(startIndex, eindIndex - (startIndex + 1)));
         }
     }
 }
