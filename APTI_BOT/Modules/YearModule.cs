cff02610f8c671b284102af0af472e8de48f6e97
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-10-19 11:51:04 +0200

    Extraheer de jaar beheer

---
+++ APTI_BOT/Modules/YearModule.cs
@@ -0,0 +1,79 @@
+﻿using APTI_BOT.Common;
+using Discord;
+using Discord.Commands;
+using Discord.Rest;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace APTI_BOT.Modules
+{
+    public class YearModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly IConfigurationRoot _config;
+        private readonly DiscordSocketClient _client;
+
+        public YearModule(IConfigurationRoot config, DiscordSocketClient client)
+        {
+            _config = config;
+            _client = client;
+            _client.ReactionRemoved += RemoveYearAsync;
+        }
+
+        [Command("jaar")]
+        [Summary("Stel je jaar van de server in.")]
+        public async Task ChangeYearAsync()
+        {
+            System.Console.WriteLine("ChangeYearAsync");
+            if (Context.IsPrivate)
+            {
+                IUserMessage sent = await ReplyAsync("Kies je jaar door op één of meer van de emoji onder dit bericht te klikken.");
+                await sent.AddReactionsAsync(Emojis.emojiJaren);
+            }
+        }
+
+        public async Task RemoveYearAsync(Cacheable<IUserMessage, ulong> msg, ISocketMessageChannel channel, SocketReaction reaction)
+        {
+            if (reaction.User.Value.IsBot)
+            {
+                return;
+            }
+
+            System.Console.WriteLine("RemoveYearAsync");
+            if (channel is IPrivateChannel)
+            {
+
+                SocketGuild _guild = _client.GetGuild(ulong.Parse(_config["ids:server"]));
+                SocketRole role;
+
+                if (reaction.Emote.Equals(Emojis.JAAR_1_EMOJI))
+                {
+                    role = _guild.GetRole(ulong.Parse(_config["ids:jaar1rol"]));
+                }
+                else if (reaction.Emote.Equals(Emojis.JAAR_2_EMOJI))
+                {
+                    role = _guild.GetRole(ulong.Parse(_config["ids:jaar2rol"]));
+                }
+
+                else if (reaction.Emote.Equals(Emojis.JAAR_3_EMOJI))
+                {
+                    role = _guild.GetRole(ulong.Parse(_config["ids:jaar3rol"]));
+                }
+                else
+                {
+                    role = null;
+                }
+
+                if (role != null)
+                {
+                    await _guild.GetUser(reaction.UserId).RemoveRoleAsync(role);
+                }
+            }
+        }
+    }
+}
