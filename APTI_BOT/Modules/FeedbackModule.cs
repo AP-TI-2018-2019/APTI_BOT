a9ee42cb7e4bd5f3e6463226ff41fa2382bf24ad
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-10-30 13:54:58 +0100

    Start feedback module voor server en studentenvert

---
+++ APTI_BOT/Modules/FeedbackModule.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+
+namespace APTI_BOT.Modules
+{
+    [Name("Feedback commando's")]
+    public class FeedbackModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly IConfigurationRoot _config;
+        private readonly DiscordSocketClient _client;
+
+        public AptiModule(IConfigurationRoot config, DiscordSocketClient client)
+        {
+            _config = config;
+            _client = client;
+        }
+    }
+}
